a1d325f09d194a821ea19b4b98d47f22b6a6d2a0
Maxence Yang <jyunanyang@gmail.com>
2025-05-07 15:56:30 +0800

    feat: add header check for both protocol

--- AppService/src/main/kotlin/idv/maxence2997/appservice/HelloController.kt
+++ AppService/src/main/kotlin/idv/maxence2997/appservice/HelloController.kt
@@ -1,15 +1,20 @@
 package idv.maxence2997.appservice
 
+import jakarta.servlet.http.HttpServletRequest
 import java.time.LocalDateTime
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController{
+class HelloController {
 
     @GetMapping("/hello")
-    fun hello(): String {
+    fun hello(request: HttpServletRequest): String {
+        val token = request.getHeader("X-Auth-Token") ?: error("X-Auth-Token header not found")
+        val userId = request.getHeader("X-User-Id") ?: error("X-User-Id header not found")
+        val orgId = request.getHeader("X-Org-Id") ?: error("X-Org-Id header not found")
+        val username = request.getHeader("X-User-Username") ?: ""
 
-        return "reply Server says hi! at ${LocalDateTime.now()}, testing envoy-gateway for REST API"
+        return "Hello $username($userId) from org $orgId, token=$token at ${LocalDateTime.now()}, testing envoy-gateway for REST API"
     }
 }
--- AppService/src/main/kotlin/idv/maxence2997/appservice/SocketIoServerInitializer.kt
+++ AppService/src/main/kotlin/idv/maxence2997/appservice/SocketIoServerInitializer.kt
@@ -20,10 +20,22 @@ class SocketIoServerInitializer(
             "req-testing-envoy-gateway-socketio",
             String::class.java,
             DataListener { client, data, ackSender ->
-                println("Received message: $data")
+                // 1. 拿到原始的 HTTP headers
+                val headers = client.handshakeData.httpHeaders
+
+                val token = headers.get("X-Auth-Token")
+                    ?: error("X-Auth-Token header not found")
+                val userId = headers.get("X-User-Id")
+                    ?: error("X-User-Id header not found")
+                val orgId = headers.get("X-Org-Id")
+                    ?: error("X-Org-Id header not found")
+                val username = headers.get("X-User-Username") ?: ""
+
+                println("Received message: $data from userId=$userId, token=$token")
+
                 client.sendEvent(
                     "resp-testing-envoy-gateway-socketio",
-                    "Server says hi! at ${LocalDateTime.now()} , testing envoy-gateway for Socket.IO"
+                    "Hello $username($userId) from org $orgId, token=$token at ${LocalDateTime.now()} , testing envoy-gateway for Socket.IO"
                 )
             })
         server.start()
