a0e03b11311d8b2e6717e08726bf9431277643ce
Maxence <maxence.otp@gmail.com>
2025-06-13 11:15:23 +0800

    feat: add socketio lib into project

---
+++ socketio-manager/src/main/kotlin/idv/maxence2997/socketiomanager/SocketIoAutoConfig.kt
@@ -0,0 +1,23 @@
+package idv.maxence2997.socketiomanager
+
+import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketIOServer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration as SpringConfiguration
+
+@SpringConfiguration
+class SocketIoAutoConfig {
+    @Bean
+    fun socketConfig(
+        @Value("\${socket.host}") host: String,
+        @Value("\${socket.port}") port: Int,
+    ): Configuration =
+        Configuration().apply {
+            this.hostname = host
+            this.port = port
+        }
+
+    @Bean
+    fun socketServer(cfg: Configuration): SocketIOServer = SocketIOServer(cfg)
+}
---
+++ socketio-manager/src/main/kotlin/idv/maxence2997/socketiomanager/SocketIoManagerApplication.kt
@@ -0,0 +1,11 @@
+package idv.maxence2997.socketiomanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocketIoManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocketIoManagerApplication>(*args)
+}
